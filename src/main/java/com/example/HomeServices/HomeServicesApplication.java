0aeba845c29b04c94e898c018ab6d68ce3229a90
cassie <gkk080805@mail.ru>
2025-12-09 23:12:42 +0300

    Старт

---
+++ src/main/java/com/example/HomeServices/HomeServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.HomeServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServicesApplication.class, args);
+	}
+
+}
