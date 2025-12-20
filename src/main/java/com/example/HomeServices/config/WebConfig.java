88809df0a227384e682b1f81582103fd26c4ce1a
pvl6a <polina.ilyuchik@gmail.com>
2025-12-20 21:05:58 +0300

    корректное создание заявки

--- src/main/java/com/example/HomeServices/config/WebConfig.java
+++ src/main/java/com/example/HomeServices/config/WebConfig.java
@@ -1,11 +1,32 @@
 package com.example.HomeServices.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Paths;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${file.upload-dir:./uploads/news_photos}")
+    private String uploadDir;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Преобразуем путь для доступа к файлам
+        String absolutePath = Paths.get(uploadDir).toAbsolutePath().toString();
+
+        // Для Windows: заменяем обратные слеши
+        String fileUrl = "file:" + absolutePath.replace("\\", "/") + "/";
+
+        // Настраиваем доступ к файлам через URL /uploads/**
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations(fileUrl);
+
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
